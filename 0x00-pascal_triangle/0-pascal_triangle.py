22bf42961e81f3f44ba0340533091d7989906c95
colly-m <sindanicolly@gmail.com>
2024-04-24 14:45:53 -0700

    Pascal algorithm fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,6 @@ Module to create function to return list of integer list
 representing pascals triangle
 """
 
-
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -14,8 +13,7 @@ def pascal_triangle(n):
     for r in range(1, n):
         row = [1]
         for g in range(1, r):
-            if g < len(tri[r - 1]):
-                row.append(tri[r - 1][g - 1] + tri[r - 1][g])
+            row.append(tri[r - 1][g - 1] + tri[r - 1][g])
 
         row.append(1)
         tri.append(row)
