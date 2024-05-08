ca520b7b234481057545feed4b247c55ccfc73ba
colly-m <sindanicolly@gmail.com>
2024-05-08 13:53:57 -0700

    Operation mini fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,20 +6,10 @@ def minOperations(n):
     """
     Function to calculate the least opeartions needed to result exactly
     as characters in file
+    :param n: The target number of H chars
     """
-    opts = 0
-    cur_chs = 1
-    clipboard = 0
-
-    while cur_chs < n:
-        if cur_chs * 2 > n and clipboard != 0:
-            opts += 1
-            cur_chs += clipboard
-        elif cur_chs * 2 <= n:
-            opts += 2
-            clipboard = cur_chs
-            cur_chs *= 2
-        else:
-            return 0
-
-    return opts
+    if n <= 1:
+        return 0
+    for opt in range(2, n+1):
+        if n % opt == 0:
+            return minOperations(int(n/opt)) + opt
