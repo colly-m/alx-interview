47b1cfd3946e29be29bdd05e69f0580151b899ee
colly-m <sindanicolly@gmail.com>
2024-08-20 06:28:02 -0700

    How change comes about

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module to determine amount of less coins to reach a given account"""
+
+
+def makeChange(coins, total):
+    """Function to check the needed coins"""
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for c in coins:
+        for r in range(c, total + 1):
+            dp[r] = min(dp[r], dp[r - c] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == '__main__':
+
+    print(makeChange([1, 2, 25], 37))
+
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
